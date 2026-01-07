f0a51833333a00ac7b1b9c9ad5b64afa56bab04f
Didarul Shahriar <didarul13579@gmail.com>
2026-01-07 20:54:40 +0600

    problem 2

--- Problem2.js
+++ Problem2.js
@@ -1,1 +1,11 @@
-let count
+const countVowel = (str) => {
+    let count = 0;
+    for(let i = 0; i<str.length; i++){
+        if(str[i].toUpperCase() == "A" || str[i].toUpperCase() == "E" || str[i].toUpperCase() == "I" || str[i].toUpperCase() == "O" || str[i].toUpperCase() == "U"){
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+countVowel("programming");
