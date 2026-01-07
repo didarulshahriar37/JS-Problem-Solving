40cb87b170d4ee50cdedc83847f65cad67876943
Didarul Shahriar <didarul13579@gmail.com>
2026-01-07 20:45:16 +0600

    added Problem1

---
+++ Problem1.js
@@ -0,0 +1,9 @@
+let reverseString = (str) =>{
+    let reverse = "";
+    for(let i = str.length-1; i>=0; i--){
+        reverse = reverse + str[i];
+    }
+    console.log(reverse);
+}
+
+reverseString("hello");
